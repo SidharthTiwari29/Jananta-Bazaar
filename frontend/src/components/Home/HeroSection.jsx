77449f46d25382829f0fd875c78ff9c27d9c2a0f
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-01 20:06:42 +0530

    Update HeroSection.jsx

--- frontend/src/components/Home/HeroSection.jsx
+++ frontend/src/components/Home/HeroSection.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import familyImage from "../../assets/images/family-banner.jpg";
+
+const HeroSection = () => {
+  return (
+    <section className="relative">
+      <img src={familyImage} alt="Family" className="w-full h-80 object-cover" />
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+        <h1 className="text-4xl font-bold">Welcome to Janata Bazaar</h1>
+        <p className="mt-2">The best deals at unbeatable prices.</p>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
 
