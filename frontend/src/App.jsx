df0dffb92aef63454461e3e59dbd31d0f31583b8
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-01 20:16:42 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,37 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes"; // App Routes (Routing for pages)
-import Navbar from "./components/common/Navbar";
-import Footer from "./components/common/Footer";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Terms from './pages/Terms';
+import HelpCenter from './pages/HelpCenter';
+import BiddingPage from './pages/BiddingPage';
+import Cart from './pages/Cart';
+import Lottery from './pages/Lottery';
+import Donation from './pages/Donation';
+import Subscription from './pages/Subscription';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <AppRoutes />
+    <div className="min-h-screen flex flex-col bg-white">
+      <Header />
+      <main className="flex-1 mt-16">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/bidding" element={<BiddingPage />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/lottery" element={<Lottery />} />
+          <Route path="/donation" element={<Donation />} />
+          <Route path="/subscription" element={<Subscription />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/help" element={<HelpCenter />} />
+          <Route path="/terms" element={<Terms />} />
+        </Routes>
+      </main>
       <Footer />
-    </Router>
+    </div>
   );
-}
+};
 
 export default App;
-
