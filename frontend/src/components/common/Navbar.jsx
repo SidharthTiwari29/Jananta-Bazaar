934fe8f96f34922c7c728b8c976d470b553ba937
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-01 20:05:36 +0530

    Update Navbar.jsx

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,1 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-primary p-4 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-3xl font-bold">Janata Bazaar</Link>
+        <div className="space-x-4">
+          <Link to="/bidding" className="text-white">Bidding</Link>
+          <Link to="/lottery" className="text-white">Lottery</Link>
+          <Link to="/wishlist" className="text-white">Wishlist</Link>
+          <Link to="/login" className="text-white">Login</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
 
