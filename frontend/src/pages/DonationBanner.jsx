6bc164c5b0fb53a79af402d49cad1c8b4ed0d8e8
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-01 20:09:23 +0530

    Update DonationBanner.jsx

--- frontend/src/pages/DonationBanner.jsx
+++ frontend/src/pages/DonationBanner.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const DonationBanner = () => {
+  return (
+    <div className="bg-green-600 text-white py-4 text-center">
+      <h2 className="text-xl">Support the Needy and Elderly</h2>
+      <p>Your donations make a big difference. Donate today!</p>
+      <Link to="/donation" className="text-lg underline">Donate Now</Link>
+    </div>
+  );
+};
+
+export default DonationBanner;
 
