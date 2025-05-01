b41b8c782161f983eba642a4df5e9df1de483e77
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-01 20:02:59 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes"; // App Routes (Routing for pages)
+import Navbar from "./components/common/Navbar";
+import Footer from "./components/common/Footer";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <AppRoutes />
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
 
