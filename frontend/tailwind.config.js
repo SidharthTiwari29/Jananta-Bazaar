034195f6d899a7f170824711c3084d824424f983
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-01 20:01:42 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,1 +1,15 @@
 
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Ensure that TailwindCSS can find all your component files
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff6f00',   // Orange
+        secondary: '#009933', // Green
+      },
+    },
+  },
+  plugins: [],
+}
