e94184e3dbc8008e0f38887d88b243a5db41550a
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-01 20:03:56 +0530

    Update AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,1 +1,32 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Bidding from "../pages/Bidding";
+import Payment from "../pages/Payment";
+import Lottery from "../pages/Lottery";
+import ContactUs from "../pages/ContactUs";
+import TermsAndConditions from "../pages/TermsAndConditions";
+import NotFound from "../pages/NotFound";
+import Wishlist from "../pages/Wishlist";
+import HelpCenter from "../pages/HelpCenter";
+import Login from "../pages/Login";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/bidding" element={<Bidding />} />
+      <Route path="/payment" element={<Payment />} />
+      <Route path="/lottery" element={<Lottery />} />
+      <Route path="/wishlist" element={<Wishlist />} />
+      <Route path="/contact-us" element={<ContactUs />} />
+      <Route path="/terms" element={<TermsAndConditions />} />
+      <Route path="/help" element={<HelpCenter />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
 
