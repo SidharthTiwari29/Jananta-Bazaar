3f45b738997f517f45bd7959987044c52cbfa347
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-01 20:07:15 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,1 +1,23 @@
+import React from "react";
+import HeroSection from "../components/home/HeroSection";
+import DealsOfTheDay from "../components/home/DealsOfTheDay";
+import FunnyQuotes from "../components/home/FunnyQuotes";
+import LotteryHighlights from "../components/home/LotteryHighlights";
+import SubscriptionBanner from "../components/home/SubscriptionBanner";
+import DonationBanner from "../components/home/DonationBanner";
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSection />
+      <DealsOfTheDay />
+      <FunnyQuotes />
+      <LotteryHighlights />
+      <SubscriptionBanner />
+      <DonationBanner />
+    </div>
+  );
+};
+
+export default Home;
 
