54889953fd7e214b8364b24406ff275e94b212b7
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-03 23:39:52 +0530

    Update Navbar.jsx

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,29 +1,30 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
-import cartIcon from '../../assets/icons/shopping-cart.svg';
 import logo from '../../assets/logo/janata-logo.png';
+import cartIcon from '../../assets/icons/shopping-cart.svg';
 
 const Navbar = () => {
   return (
-    <header className="bg-white shadow-md sticky top-0 z-50">
-      <div className="container mx-auto px-4 py-3 flex justify-between items-center">
-        <Link to="/" className="flex items-center">
-          <img src={logo} alt="Janata Bazaar" className="h-10 w-auto mr-2" />
+    <nav className="bg-white shadow-md sticky top-0 z-50">
+      <div className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
+        <Link to="/" className="flex items-center gap-2">
+          <img src={logo} alt="Janata Bazaar Logo" className="h-10" />
           <span className="text-2xl font-bold text-orange-600">Janata Bazaar</span>
         </Link>
-        <nav className="flex items-center space-x-6">
-          <Link to="/bidding" className="text-gray-700 hover:text-orange-500 font-medium">Bidding</Link>
-          <Link to="/deals" className="text-gray-700 hover:text-orange-500 font-medium">Deals</Link>
-          <Link to="/lottery" className="text-gray-700 hover:text-orange-500 font-medium">Lottery</Link>
-          <Link to="/coins" className="text-gray-700 hover:text-orange-500 font-medium">Coins</Link>
-          <Link to="/subscription" className="text-gray-700 hover:text-orange-500 font-medium">Subscription</Link>
-          <Link to="/wishlist" className="text-gray-700 hover:text-orange-500 font-medium">Wishlist</Link>
-          <Link to="/profile" className="text-gray-700 hover:text-orange-500 font-medium">My Profile</Link>
-          <Link to="/cart" className="relative">
-            <img src={cartIcon} alt="Cart" className="h-6 w-6" />
-          </Link>
-        </nav>
+        
+        <ul className="flex gap-6 text-gray-700 font-medium items-center">
+          <li><Link to="/bidding">Bidding</Link></li>
+          <li><Link to="/lottery">Lottery</Link></li>
+          <li><Link to="/subscription">Subscription</Link></li>
+          <li><Link to="/wallet">Wallet</Link></li>
+          <li><Link to="/donate">Donate</Link></li>
+          <li><Link to="/profile">Profile</Link></li>
+          <li><Link to="/cart">
+            <img src={cartIcon} alt="Cart" className="h-6" />
+          </Link></li>
+        </ul>
       </div>
-    </header>
+    </nav>
   );
 };
 
