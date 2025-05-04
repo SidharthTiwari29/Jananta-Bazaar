b225afa3ccb6f4d61005472b18d67ba530f191b9
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-04 13:11:03 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,1 +1,24 @@
+// utils/sendEmail.js
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendEmail = async (to, subject, text, html) => {
+  const msg = {
+    to,
+    from: process.env.SENDGRID_SENDER_EMAIL, // Your SendGrid email
+    subject,
+    text,
+    html,
+  };
+
+  try {
+    await sgMail.send(msg);
+    console.log('Email sent');
+  } catch (error) {
+    console.error('Error sending email', error);
+  }
+};
+
+module.exports = sendEmail;
 
