e9561f5f4c311b6dfa92bf9d2053ad42d5f82d15
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-04 12:06:15 +0530

    Update contactUs.jsx

--- frontend/src/pages/contactUs.jsx
+++ frontend/src/pages/contactUs.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+
+const ContactUs = () => (
+  <div className="p-6 max-w-xl mx-auto">
+    <h1 className="text-2xl font-bold mb-2">Contact Us</h1>
+    <p>Email: support@janatabazaar.in</p>
+    <p>Phone: +91-98765-43210</p>
+    {/* TODO: Add contact form */}
+  </div>
+);
+
+export default ContactUs;
 
