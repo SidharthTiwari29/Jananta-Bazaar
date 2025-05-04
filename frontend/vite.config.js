745fd19d20c04b8a964ca16b6dfe912cf622468f
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-04 12:45:40 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://api.janata-bazaar.in',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
 
