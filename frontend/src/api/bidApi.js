ce3cb495f5d52befde53a2d3c2c84071c05a6786
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-04 13:43:39 +0530

    Update bidApi.js

--- frontend/src/api/bidApi.js
+++ frontend/src/api/bidApi.js
@@ -1,1 +1,26 @@
+import API from './axios';
+
+// Place a new bid
+export const placeBid = async (productId, bidValue) => {
+  const res = await API.post('/bids/place', { productId, bidValue });
+  return res.data;
+};
+
+// Get all bids by user
+export const getMyBids = async () => {
+  const res = await API.get('/bids/my');
+  return res.data;
+};
+
+// Validate a bid (client-side validation happens in utils, this hits backend)
+export const validateBid = async (productId, bidValue) => {
+  const res = await API.post('/bids/validate', { productId, bidValue });
+  return res.data;
+};
+
+// Get live bid data for a product
+export const getProductBidStatus = async (productId) => {
+  const res = await API.get(`/bids/status/${productId}`);
+  return res.data;
+};
 
