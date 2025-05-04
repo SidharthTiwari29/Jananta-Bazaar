39739b189a72f153d558eeb300d1fb00f94297a7
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-04 12:35:46 +0530

    Update coinRewardCalc.js

--- frontend/src/utils/coinRewardCalc.js
+++ frontend/src/utils/coinRewardCalc.js
@@ -1,1 +1,11 @@
+// coinrewardcalc.js
+export const calculateCoins = (amount) => {
+  return Math.floor(amount / 10); // ₹10 = 1 coin
+};
 
+export const getDiscountFromCoins = (coins) => {
+  if (coins >= 500) return 850;
+  if (coins >= 250) return 500;
+  if (coins >= 100) return 200;
+  return 0;
+};
