401ebf4f15cee386e465e2eb8fe1bf4cb3ff62dd
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-04 12:06:45 +0530

    Update Wishlist.jsx

--- frontend/src/pages/Wishlist.jsx
+++ frontend/src/pages/Wishlist.jsx
@@ -1,1 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import axios from '../utils/axiosInstance';
+
+const Wishlist = () => {
+  const [wishlist, setWishlist] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/user/wishlist')
+      .then(res => setWishlist(res.data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Wishlist</h1>
+      <ul className="space-y-2">
+        {wishlist.map((item, index) => (
+          <li key={index} className="p-3 border rounded shadow">{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Wishlist;
 
