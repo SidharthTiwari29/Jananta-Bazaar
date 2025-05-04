d119bcfd3d88e29ed048f5d234d74c25610866f8
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-04 13:42:37 +0530

    Update userApi.js

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,1 +1,26 @@
+import API from './axios';
+
+// Fetch user profile (name, email, coins, etc.)
+export const fetchUserProfile = async () => {
+  const res = await API.get('/user/profile');
+  return res.data;
+};
+
+// Fetch wallet balance and history
+export const fetchWallet = async () => {
+  const res = await API.get('/user/wallet');
+  return res.data;
+};
+
+// Fetch user's rewards history
+export const fetchRewards = async () => {
+  const res = await API.get('/user/rewards');
+  return res.data;
+};
+
+// Fetch donation receipts
+export const fetchDonationReceipts = async () => {
+  const res = await API.get('/user/donations');
+  return res.data;
+};
 
