bca253f936675e6dee4f739397daca9815014e30
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-04 13:41:45 +0530

    Update authApi.js

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,1 +1,26 @@
+import API from './axios';
+
+// Login user
+export const loginUser = async (credentials) => {
+  const res = await API.post('/auth/login', credentials);
+  return res.data;
+};
+
+// Signup user
+export const signupUser = async (userData) => {
+  const res = await API.post('/auth/signup', userData);
+  return res.data;
+};
+
+// Logout user
+export const logoutUser = async () => {
+  const res = await API.post('/auth/logout');
+  return res.data;
+};
+
+// Get current user profile (if JWT token exists)
+export const getCurrentUser = async () => {
+  const res = await API.get('/auth/me');
+  return res.data;
+};
 
