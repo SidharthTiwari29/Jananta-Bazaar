5b4f5835575446dcb8c99e1a36bf9d36a722d66d
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-04 13:26:53 +0530

    Update Bid.js

--- backend/models/Bid.js
+++ backend/models/Bid.js
@@ -1,1 +1,33 @@
+// backend/models/Bid.js
+const mongoose = require('mongoose');
+
+const bidSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  bidAmount: {
+    type: Number,
+    required: true,
+  },
+  bidTime: {
+    type: Date,
+    default: Date.now,
+  },
+  status: {
+    type: String,
+    enum: ['active', 'expired', 'won'],
+    default: 'active',
+  },
+});
+
+const Bid = mongoose.model('Bid', bidSchema);
+
+module.exports = Bid;
 
