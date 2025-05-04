c3c3cb2a58b7c7e5da4d277423c1db490492179f
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-04 13:06:44 +0530

    Update errorHandler.js

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,1 +1,10 @@
+// middleware/errorHandler.js
+const errorHandler = (err, req, res, next) => {
+  console.error(err);
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Server Error';
+  res.status(statusCode).json({ message });
+};
+
+module.exports = { errorHandler };
 
