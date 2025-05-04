3b22aa9419e38bb61d99d138cdfab756c2dcf422
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-04 13:43:00 +0530

    Update subscriptionApi.js

--- frontend/src/api/subscriptionApi.js
+++ frontend/src/api/subscriptionApi.js
@@ -1,1 +1,20 @@
+import API from './axios';
+
+// Get current subscription status
+export const getSubscriptionStatus = async () => {
+  const res = await API.get('/subscription/status');
+  return res.data;
+};
+
+// Start new subscription
+export const startSubscription = async () => {
+  const res = await API.post('/subscription/start');
+  return res.data;
+};
+
+// Renew subscription
+export const renewSubscription = async () => {
+  const res = await API.post('/subscription/renew');
+  return res.data;
+};
 
