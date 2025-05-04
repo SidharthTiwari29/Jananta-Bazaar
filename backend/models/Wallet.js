b4b01c6ad9593595d1f82b66f5f55f1fe4c4ebd5
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-04 13:28:48 +0530

    Update Wallet.js

--- backend/models/Wallet.js
+++ backend/models/Wallet.js
@@ -1,1 +1,23 @@
+// backend/models/Wallet.js
+const mongoose = require('mongoose');
+
+const walletSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  balance: {
+    type: Number,
+    default: 0,
+  },
+  transactions: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Payment',
+  }],
+});
+
+const Wallet = mongoose.model('Wallet', walletSchema);
+
+module.exports = Wallet;
 
