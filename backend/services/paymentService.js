2537b28a9de660ab67fe5b7efae6ccd8e91b38ea
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-04 13:07:47 +0530

    Update paymentService.js

--- backend/services/paymentService.js
+++ backend/services/paymentService.js
@@ -1,1 +1,19 @@
+// services/paymentService.js
+const Stripe = require('stripe');
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+const createPaymentIntent = async (amount) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount * 100, // Amount in cents
+      currency: 'inr',
+      payment_method_types: ['card'],
+    });
+    return paymentIntent.client_secret;
+  } catch (err) {
+    throw new Error('Error creating payment intent');
+  }
+};
+
+module.exports = { createPaymentIntent };
 
