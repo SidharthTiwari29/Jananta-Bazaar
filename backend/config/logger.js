3c68e82c05fe3a6e1d1e8bd965caeecef93678d6
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-04 13:08:56 +0530

    Update logger.js

--- backend/config/logger.js
+++ backend/config/logger.js
@@ -1,1 +1,14 @@
+// config/logger.js
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'app.log' })
+  ]
+});
+
+module.exports = logger;
 
