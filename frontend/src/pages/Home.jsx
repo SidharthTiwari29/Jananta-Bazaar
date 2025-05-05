49350a11067b7741ba921698b0ccce7f8129bd4d
SidharthTiwari29 <Sidharth2906@gmail.com>
2025-05-05 09:12:11 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,13 +3,23 @@ import { motion } from "framer-motion";
 import HeroSection from "../components/Home/HeroSection";
 import OfferOverview from "../components/Home/OfferOverview";
 import DealsSection from "../components/Home/DealsSection";
-import WinnersSection from "../components/Home/WinnersSection";
 import LiveBargains from "../components/Home/LiveBargains";
+import Leaderboard from "../components/Home/Leaderboard";
+import BiddingClock from "../components/Home/BiddingClock";
+import DealsOfTheDay from "../components/Home/DealsOfTheDay";
+import DonationBanner from "../components/Home/DonationBanner";
+import FestiveThemeSwitcher from "../components/Home/FestiveThemeSwitcher";
+import FunnyQuotes from "../components/Home/FunnyQuotes";
+import LotteryHighlights from "../components/Home/LotteryHighlights";
+import SubscriptionBanner from "../components/Home/SubscriptionBanner";
 
 const Home = () => {
   return (
     <main className="bg-orange-50 min-h-screen font-inter">
       <HeroSection />
+
+      {/* Festive Theme Switcher */}
+      <FestiveThemeSwitcher />
 
       {/* Humorous Quote */}
       <section className="text-center py-8 bg-orange-100 text-gray-800">
@@ -18,24 +28,51 @@ const Home = () => {
         </p>
       </section>
 
+      <FunnyQuotes />
+
+      {/* Bidding Countdown Clock */}
+      <section className="max-w-7xl mx-auto px-4 py-6">
+        <BiddingClock />
+      </section>
+
       {/* What Janata Bazaar Offers */}
       <section className="max-w-7xl mx-auto px-4 py-10">
         <OfferOverview />
       </section>
 
-      {/* Deals of the Day and Trending */}
+      {/* Deals of the Day */}
+      <section className="max-w-7xl mx-auto px-4 py-10">
+        <DealsOfTheDay />
+      </section>
+
+      {/* Trending & Popular Deals */}
       <section className="max-w-7xl mx-auto px-4 py-10">
         <DealsSection />
       </section>
 
-      {/* Daily Winners */}
+      {/* Lottery Highlights */}
       <section className="max-w-7xl mx-auto px-4 py-10">
-        <WinnersSection />
+        <LotteryHighlights />
+      </section>
+
+      {/* Leaderboard (Replaces WinnersSection) */}
+      <section className="max-w-7xl mx-auto px-4 py-10">
+        <Leaderboard />
+      </section>
+
+      {/* Donation Banner */}
+      <section className="max-w-7xl mx-auto px-4 py-10">
+        <DonationBanner />
       </section>
 
       {/* Live Bargains */}
       <section className="max-w-7xl mx-auto px-4 py-10">
         <LiveBargains />
+      </section>
+
+      {/* Newsletter / Subscription */}
+      <section className="max-w-7xl mx-auto px-4 py-10">
+        <SubscriptionBanner />
       </section>
     </main>
   );
